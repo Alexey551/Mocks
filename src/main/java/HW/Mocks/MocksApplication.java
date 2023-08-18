51a06bfabcee66d00901de8fd8030ed4df674208
Alexey <pap220979@mail.ru>
2023-08-18 12:51:24 +0500

    delete property

--- src/main/java/HW/Mocks/MocksApplication.java
+++ src/main/java/HW/Mocks/MocksApplication.java
@@ -3,6 +3,7 @@ package HW.Mocks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MocksApplication {
 
